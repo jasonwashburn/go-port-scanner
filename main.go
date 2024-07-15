807c31fd7db265ff960cdf6a837b49ff417be9da
Jason Washburn <jason.washburn@gmail.com>
2024-07-15 08:47:12 -0500

    add worker pool with results channel

--- main.go
+++ main.go
@@ -3,23 +3,47 @@ package main
 import (
 	"fmt"
 	"net"
-	"sync"
+	"sort"
 )
 
-func main() {
-	var wg sync.WaitGroup
-	for i := 1; i <= 1024; i++ {
-		wg.Add(1)
-		go func(j int) {
-			defer wg.Done()
-			address := fmt.Sprintf("scanme.nmap.org:%d", j)
-			conn, err := net.Dial("tcp", address)
-			if err != nil {
-				return
-			}
-			fmt.Printf("%s - Connection successful\n", address)
-			conn.Close()
-		}(i)
+func worker(ports chan int, results chan int) {
+	for p := range ports {
+		conn, err := net.Dial("tcp", fmt.Sprintf("scanme.nmap.org:%d", p))
+		if err != nil {
+			results <- 0
+			continue
+		}
+		conn.Close()
+		results <- p
 	}
-	wg.Wait()
+}
+
+func main() {
+	var openPorts []int
+	ports := make(chan int, 100)
+	results := make(chan int, 100)
+	defer close(ports)
+	defer close(results)
+
+	for i := 0; i < cap(ports); i++ {
+		go worker(ports, results)
+	}
+
+	go func() {
+		for i := 1; 1 <= 1024; i++ {
+			ports <- i
+		}
+	}()
+
+	for i := 0; i <= 1024; i++ {
+		port := <-results
+		if port != 0 {
+			openPorts = append(openPorts, port)
+		}
+	}
+
+	sort.Ints(openPorts)
+	for _, port := range openPorts {
+		fmt.Printf("%d open\n", port)
+	}
 }
