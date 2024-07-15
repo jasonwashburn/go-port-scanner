f4ae1b3bb825d3e784d281cbc451cc34e3fb7cb4
Jason Washburn <jason.washburn@gmail.com>
2024-07-15 07:47:34 -0500

    add go routines

--- main.go
+++ main.go
@@ -3,16 +3,23 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 func main() {
+	var wg sync.WaitGroup
 	for i := 1; i <= 1024; i++ {
-		address := fmt.Sprintf("scanme.nmap.org:%d", i)
-		conn, err := net.Dial("tcp", address)
-		if err != nil {
-			continue
-		}
-		fmt.Printf("%s - Connection successful\n", address)
-		conn.Close()
+		wg.Add(1)
+		go func(j int) {
+			defer wg.Done()
+			address := fmt.Sprintf("scanme.nmap.org:%d", j)
+			conn, err := net.Dial("tcp", address)
+			if err != nil {
+				return
+			}
+			fmt.Printf("%s - Connection successful\n", address)
+			conn.Close()
+		}(i)
 	}
+	wg.Wait()
 }
