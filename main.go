3865ec023b8cf6b0ed6fc1bd9d48e2e4661fa406
Jason Washburn <jason.washburn@gmail.com>
2024-07-15 07:29:47 -0500

    add simple scan of ports 1-1024

--- main.go
+++ main.go
@@ -6,9 +6,13 @@ import (
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "scanme.nmap.org:80")
-	if err == nil {
-		fmt.Println("Connection successful")
+	for i := 1; i <= 1024; i++ {
+		address := fmt.Sprintf("scanme.nmap.org:%d", i)
+		conn, err := net.Dial("tcp", address)
+		if err != nil {
+			continue
+		}
+		fmt.Printf("%s - Connection successful\n", address)
+		conn.Close()
 	}
-	defer conn.Close()
 }
